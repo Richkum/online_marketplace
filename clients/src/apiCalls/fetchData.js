9e9c52fbac6197e407882930704c11c9c95fd2d4
Richkum <kumatum2@gmail.com>
2024-08-01 14:08:53 +0100

    commiting

--- clients/src/apiCalls/fetchData.js
+++ clients/src/apiCalls/fetchData.js
@@ -85,4 +85,34 @@ const fetchUserProducts = async (userId) => {
   }
 };
 
-export { fetchCategories, fetchProducts, fetchReviews, fetchUserProducts };
+const fetchSearchProducts = async (keyword) => {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get(
+      `http://localhost:3000/product/search-products?keyword=${keyword}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching products:", {
+      message: error.message,
+      config: error.config,
+      request: error.request,
+      response: error.response,
+    });
+    throw error;
+  }
+};
+
+export {
+  fetchCategories,
+  fetchProducts,
+  fetchReviews,
+  fetchUserProducts,
+  fetchSearchProducts,
+};
